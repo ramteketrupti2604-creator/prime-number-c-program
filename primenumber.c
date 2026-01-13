876d00e1bcf53d7bf1e2a2328fb21bf4f4bb9bbd
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 11:57:11 +0530

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n=57;
+    if(n%2!=0){
+        printf("number is prime");
+    }
+    else{
+        printf("number is not prime");
+    }
+    return 0;
+}
